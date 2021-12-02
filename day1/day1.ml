19e025fb3e69255c689a33af382aeb63386b73cf
Filip Doversten <filip@doversten.se>
2021-12-02 12:09:43 +0100

    Cleaned up Day1 Part 1 and 2
    * Removed left-over out-commented code and debug printings
    * Added description/explanation of the "overlapping_chunks_of" function
    * Added explanation of the different alternative Part1 solutions and
      why I prefer "part1b" over "part1a"

--- day1/day1.ml
+++ day1/day1.ml
@@ -19,11 +19,8 @@ let count_in_pairs list f =
     let rec inner hd tl accum =
         match tl with
         | [] -> accum
-        | [last] -> 
-            print_string ("hd = " ^ (string_of_int hd) ^ " last = " ^ (string_of_int last) ^ " accum = " ^ (string_of_int accum) ^ "\n");
-            Bool.to_int(f hd last) + accum
+        | [last] -> Bool.to_int(f hd last) + accum
         | next_hd::next_tl -> 
-            print_string ("hd = " ^ (string_of_int hd) ^ " next_hd = " ^ (string_of_int next_hd) ^ " accum = " ^ (string_of_int accum) ^ "\n");
             inner next_hd next_tl (Bool.to_int(f hd next_hd) + accum)
     in
     inner (List.hd list) (List.tl list) 0
@@ -48,21 +45,26 @@ module List = struct
 
     let sub = Base.List.sub
 
+    (* Works like Jane Steet Base's "chunks_of" function, except
+       each chunk will have elements that overlaps with the next
+       chunk.
+       You can see it as a "sliding window" that is the size of "chunk_size"
+       and starts from the beginning of the list and slides one element
+       between each chunk and each time it generates a chunk including the
+       elements that the "sliding window" at that moment includes.
+       Example:
+       "overlapping_chunks_of [1; 2; 3; 4; 5] 3"
+       would give the result
+       [[1; 2; 3]; [2; 3; 4]; [3; 4; 5]]
+
+       TODO:
+       A potential addition to this function would be to allow user to supply
+       how much the window should slide in bewteen capturing each chunk,
+       if the user would want it to be more than just one step at a time.
+       *)
     let overlapping_chunks_of list chunk_size =
         let rec next_chunk pos accu =
             if ( pos + chunk_size ) > ( List.length list ) then
-                (*
-                This is what you would do if you want to include 
-                the last group if it's smaller than "chunk_size".
-
-                (* rem_len = remaining_length *)
-                let rem_len = (List.length list) - pos in
-                (sub list pos rem_len) :: accu
-
-                But for the convenience of this assignment, where
-                we don't care about this last group, we'll return
-                "accu" directly
-                *)
                 accu
             else
                 next_chunk (pos + 1) ((sub list pos chunk_size) :: accu)
@@ -72,47 +74,32 @@ module List = struct
 end
 
 (***********************************************************************)
-(*
+
+(* Difference between part1a and part1b (just two different solutions)
+
+    part1a = Converts from input line from string to int at each
+            comparison.
+
+    part1b = Converts all input lines before starting the counting/comparison
+             into integers, generating a list of all the input as a "int list"
+             before starting the comparison.
+
+    Conclusion = I like part1b better, because the comparison function we supply
+                 when calling "count_in_pairs" goes from being:
+                 - (fun a b -> int_of_string(a) < int_of_string(b))
+                 to simply
+                 - (<)
+                 in ocaml fashion!
+*)
 let part1a lines =
     let check_increase a b = int_of_string(a) < int_of_string(b) in
     let res = count_in_pairs lines check_increase in
     print_endline ("part1a result = " ^ (string_of_int res))
-*)
+
 let part1b lines =
     let numbers = List.map int_of_string lines in
     let res = count_in_pairs numbers (<) in
     print_endline ("part1b result = " ^ (string_of_int res))
-
-(*
-    OLD, first version that's incorrect, because I misinterpreted
-    the assignment and though that measurements that would get group
-    in triplets shouldn't be overlapping, so in a list like:
-    1, 2, 3, 4, 5, 6, ...
-    I thought the group should be (1, 2, 3), (4, 5, 6)
-    but it should actually be (1, 2, 3), (2, 3, 4). (3, 4, 5), ....
-
-let part2 lines =
-    (* A "number" here refers to a single sonar measure,
-       in the type 'int'.
-       See AdventOfCode assignment instructions for clarification. *)
-    let numbers = List.map ~f:int_of_string lines in
-
-    (* A "triplet" here means a "three-measurement sliding window".
-       See AdventOfCode assignment instructions for clarification. *)
-    let triplets = List.groupi numbers ~break:(fun i _ _ -> i mod 3 = 0) in
-    let fixed_triplets = 
-        if (List.length numbers) mod 3 = 0 then 
-            triplets
-        else
-            List.pop_last triplets
-    in
-
-    let triplet_sums = List.map fixed_triplets ~f:(fun a -> List.sum a) in
-
-    let _ = List.iter (fun a -> print_int a;print_string "\n") triplet_sums  in
-
-    let res = count_in_pairs triplet_sums (<) in
-    print_endline ("part2 result = " ^ (string_of_int res)) *)
 
 let part2 lines =
     (* A "number" here refers to a single sonar measure,
@@ -130,5 +117,6 @@ let part2 lines =
 
 let () = 
     let lines = input_all_lines "input.txt" in
+    part1a lines;
     part1b lines;
     part2 lines
