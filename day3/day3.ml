9a10bdc56563b731b2ffa5ccda786bca7a5569b7
Filip Doversten <filip@doversten.se>
2021-12-08 19:53:25 +0100

    Day 3 hopefully the last clean up
    * Removed the out-commented code (unused functions, debug code etc.)
    
    * Removed suppression of warning of "unused-var"
    
    * Removed unused variable "num_of_rows"
    
    * Removed the solution "Part2d" that was only started on but never finished.
    
    * Removed the solution "Part2a" that did not produce the correct answer
      and changed the naming of the other Part 2 solutions so that:
      - Part2b -> Part2a
      - Part2c -> Part2b
    
    * Changed name on the function "most_common_bit" to "get_most_common_bit".
      I noticed that I used the same name of that function as the name of it's
      returned value in Part2b and Part2c (From now on renamed to Part2a and Part2b).
      It did still seem to function perfectly well despite that, but still... it
      looked really wrong so I ended up changing it.

--- day3/day3.ml
+++ day3/day3.ml
@@ -12,7 +12,6 @@ let input_all_lines file =
 module String = struct
     include String
     let to_list = Base.String.to_list
-    (*let to_array = Base.String.to_array*)
 end
 
 module List = struct
@@ -36,22 +35,11 @@ module List = struct
         next_el list (Hashtbl.create (List.length list))*)
 end
 
-module Array = struct
-    include Array
-    let transpose_exn = Base.Array.transpose_exn
-end
-
 module Bool = struct
     include Bool
     let to_int_char boolean = 
         Base.Char.of_string (Int.to_string (Bool.to_int boolean))
 end
-
-(*module Int = struct
-    include Int
-    let to_char integer =
-        Base.Char.of_string (Int.to_string integer)
-end*)
 
 module Char = struct
     include Char
@@ -60,34 +48,18 @@ end
 
 (**********************************************)
 
-(* Returns TRUE if there are more '1's than '0's
-   and also TRUE if there are equal number of '1's and '0's *)
-(*let is_one_most_common bits = 
+(* If there is an equal number of 0s and 1s, it returns 1 *)
+let get_most_common_bit bits = 
     let num_of_zeroes = List.count ~f:(fun a -> a = '0') bits in
     let num_of_ones = (List.length bits) - num_of_zeroes in
-    let res =  num_of_ones >= num_of_zeroes  in
-    print_string ("  num zeroes = " ^ (string_of_int num_of_zeroes) ^ " num ones = " ^ (string_of_int num_of_ones) ^ "\n");
-    print_string ("    is_one = " ^ (Bool.to_string res) ^ "\n");
-    res
-*)
-(* If there is equal number of 0s and 1s, it returns 1 *)
-let most_common_bit bits = 
-    let num_of_zeroes = List.count ~f:(fun a -> a = '0') bits in
-    let num_of_ones = (List.length bits) - num_of_zeroes in
-    let res =  num_of_ones >= num_of_zeroes in
-    (*print_string ("  num zeroes = " ^ (string_of_int num_of_zeroes) ^ " num ones = " ^ (string_of_int num_of_ones) ^ "\n");
-    print_string ("    is_one = " ^ (Bool.to_string res) ^ "\n"); *)
-    Bool.to_int_char res
+    Bool.to_int_char (num_of_ones >= num_of_zeroes)
 
 let part1 lines =
     let list_of_rows = List.map ~f:(fun str -> String.to_list str) lines in
     let list_of_columns = List.transpose_exn list_of_rows in
-    let num_of_rows = List.length lines in
 
     let get_rates (accu_gamma, accu_epsilon) col = 
-        (*let gamma_bit_int = Bool.to_int (is_one_most_common col) in
-        let gamma_bit_str = string_of_int gamma_bit_int in*)
-        let gamma_bit_char = most_common_bit col in
+        let gamma_bit_char = get_most_common_bit col in
         let gamma_bit_str = Char.to_string gamma_bit_char in
 
         let epsilon_bit_str = if gamma_bit_str = "0" then "1" else "0" in
@@ -105,75 +77,7 @@ let part1 lines =
 
     print_string ("Part1 answer (power consump) = " ^ string_of_int (gamma_rate * epsilon_rate) ^ "\n\n")
 
-let print_str_list str_list = 
-    let rec next_str l i =
-        match l with
-        | [] -> ()
-        | h::t -> 
-            print_string ("s" ^ (string_of_int i) ^ " = " ^ h ^ "\n");
-            next_str t (i+1)
-    in
-    next_str str_list 0
-
-(* This part2a version doesn't work.
-   The problem with this one is that when it calculates the
-   "most_common_bit" it does so from the original list of binary sequences
-   instead of progressively calculate the next "most_common_bit" based
-   on the current/remaining binary sequences for o2 and co2 ratings respectively.
-   Because of this we get an incorrect final answer *)
 let part2a lines =
-    let list_of_rows = List.map ~f:(fun str -> String.to_list str) lines in
-    let list_of_columns = List.transpose_exn list_of_rows in
-
-    let parse_column (o2_list, co2_list, index) col =
-        (*let most_common_bit_char =  Bool.to_int_char (is_one_most_common col) in*)
-        let most_common_bit_char = most_common_bit col in
-        (*print_string ("------### " ^ (string_of_int index) ^ " ###------\n");
-        print_str_list o2_list; 
-        print_string "\n";
-        print_str_list co2_list;
-        print_string "\n---------------------------\n";*)
-
-        let o2_list = 
-            if List.length o2_list > 1 then
-                List.filter (fun row -> (String.get row index) = most_common_bit_char) o2_list
-            else
-                o2_list
-        in
-
-        let co2_list = 
-            if List.length co2_list > 1 then
-                List.filter (fun row -> (String.get row index) != most_common_bit_char) co2_list
-            else
-                co2_list
-        in
-
-        (*print_str_list o2_list; 
-        print_string "\n";
-        print_str_list co2_list;
-        print_string "\n";*)
-        (o2_list, co2_list, index + 1)
-    in
-    let (o2_list, co2_list, _) = 
-        List.fold_left parse_column (lines, lines, 0) list_of_columns in
-
-    print_string ("o2_list  result len = " ^ (string_of_int (List.length o2_list)) ^ "\n");
-    print_string ("co2_list result len = " ^ (string_of_int (List.length co2_list)) ^ "\n");
-
-    let o2_rating_str = List.hd o2_list in
-    let co2_rating_str = List.hd co2_list in
-    print_string ("o2  rating binary = " ^ o2_rating_str ^ "\n");
-    print_string ("co2 rating binary = " ^ co2_rating_str ^ "\n");
-
-    let o2_rating = int_of_string ("0b" ^ o2_rating_str) in
-    let co2_rating = int_of_string ("0b" ^ co2_rating_str) in
-    print_string ("o2 rating decimal = " ^ (string_of_int o2_rating) ^ "\n");
-    print_string ("co2 rating decimal = " ^ (string_of_int co2_rating) ^ "\n");
-
-    print_string ("Part 2a answer (life support rating) = " ^ string_of_int (o2_rating * co2_rating) ^ "\n\n")
-
-(* part2b, first version of part 2 that actually works *)
-let part2b lines =
     let next_filtering binseq_list bit_index is_o2_filtering =
         if (List.length binseq_list) <= 1 then
             binseq_list
@@ -181,9 +85,7 @@ let part2b lines =
             let list_of_rows = List.map ~f:(fun str -> String.to_list str) binseq_list in
             let list_of_columns = List.transpose_exn list_of_rows in
             let col = List.nth list_of_columns bit_index in
-
-            (*let most_common_bit = Bool.to_int_char (is_one_most_common col) in*)
-            let most_common_bit = most_common_bit col in
+            let most_common_bit = get_most_common_bit col in
 
             if is_o2_filtering then
                 List.filter (fun binseq -> (String.get binseq bit_index) = most_common_bit) binseq_list
@@ -210,20 +112,19 @@ let part2b lines =
     print_string ("o2 rating (decimal) = " ^ (string_of_int o2_rating) ^ "\n");
     print_string ("co2 rating (decimal) = " ^ (string_of_int co2_rating) ^ "\n");
 
-    print_string ("Part 2b answer (life support rating) = " ^ string_of_int (o2_rating * co2_rating) ^ "\n\n")
+    print_string ("Part 2a answer (life support rating) = " ^ string_of_int (o2_rating * co2_rating) ^ "\n\n")
 
-(* part2c, improvement from part2b, instead of doing a transpose everytime to
+(* part2b, improvement from part2a, instead of doing a transpose everytime to
    get a column of bits from the binary input data so that I can use to find 
    the most common bit, I do a "List.map" execution to get that column instead.
    Requires fewer steps and is hopefully more efficient. *)
-let part2c lines =
+let part2b lines =
     let filter_ratings binaries bit_index is_o2_filtering =
         if (List.length binaries) <= 1 then
             binaries
         else
             let col = List.map ~f:(fun str -> String.get str bit_index) binaries in
-            (*let most_common_bit = Bool.to_int_char (is_one_most_common col) in*)
-            let most_common_bit = most_common_bit col in
+            let most_common_bit = get_most_common_bit col in
 
             if is_o2_filtering then
                 List.filter (fun binary -> (String.get binary bit_index) = most_common_bit) binaries
@@ -251,49 +152,10 @@ let part2c lines =
     print_string ("o2 rating (decimal) = " ^ (string_of_int o2_rating) ^ "\n");
     print_string ("co2 rating (decimal) = " ^ (string_of_int co2_rating) ^ "\n");
 
-    print_string ("Part 2c answer (life support rating) = " ^ string_of_int (o2_rating * co2_rating) ^ "\n\n")
-
-(*
-Started attempt at creating a solution that turns the list of binary numbers (the input data)
-into a 2d-array using the Bigarray module in Ocaml stdlib.
-However, I stopped at the moment inside the function "filter_ratings" when I realized that
-I need a filter function that works on Bigarray type and I don't know if such a function
-already exists, at least it does not in the stdlib as far as I can tell. *)
-(* let part2d lines =
-    let lines_arr = Array.of_list lines in
-    let two_dim_arr = Array.map (fun line -> String.to_array line) lines_arr in
-    let bit_grid = Bigarray.Array2.of_array Bigarray.char Bigarray.c_layout two_dim_arr in
-
-    let filter_ratings _2d_arr bit_index is_o2_filtering =
-        if (Bigarray.Array2.dim1 _2d_arr) <= 1 then
-            _2d_arr
-        else
-            let col = List.map (fun str -> Bigarray.get str bit_index) binary in
-            (*let most_common_bit = Bool.to_int_char (is_one_most_common col) in*)
-            let most_common_bit = most_common_bit col in
-
-            if is_o2_filtering then
-                List.filter (fun binary_arr -> (binary_arr.{bit_index) = most_common_bit) _2d_arr
-            else
-                List.filter (fun binary_arr -> (String.get binary bit_index) != most_common_bit) _2d_arr
-    in
-
-    let rec next_bit o2_2d_arr co2_2d_arr index num_of_cols =
-        if index = num_of_cols || 
-           ( Bigarray.dim1 o2_2d_arr <= 1 && 
-             Bigarray.dim1 co2_2d_arr <= 1 )
-        then
-            (List.hd o2_2d_arr, List.hd co2_list)
-        else
-            let o2_2d_arr = next_filtering o2_2d_arr i true in
-            let co2_2d_arr = next_filtering co2_2d_arr i false in
-            next_bit o2_2d_arr co2_2d_arr (i+1) num_of_cols
-    in
-    next_bit bit_grid bit_grid 0 (Bigarray.dim2 bit_grid) *)
+    print_string ("Part 2b answer (life support rating) = " ^ string_of_int (o2_rating * co2_rating) ^ "\n\n")
 
 let () = 
     let lines = input_all_lines "input.txt" in
     part1 lines;
     part2a lines;
-    part2b lines;
-    part2c lines
+    part2b lines
