8b565427afa6a388c45a7fc1d2861defeb15e0c0
Filip Doversten <filip@doversten.se>
2022-01-16 12:23:26 +0100

    Day 8 Part 1 and 2 complete.. Debug and unused code is left-over

--- day8/day8.ml
+++ day8/day8.ml
@@ -2,6 +2,7 @@
 module String = struct
     include String
     let lsplit2_exn  = Base.String.lsplit2_exn 
+    let to_list = Base.String.to_list
 end
 
 module List = struct
@@ -25,9 +26,9 @@ let parse_line line accu_patterns accu_digit_outputs =
     let pattern_str, digit_output_str = 
         String.lsplit2_exn  ~on:'|' line in
     let new_pattern = 
-        String.split_on_char ' ' pattern_str in
+        String.split_on_char ' ' (String.trim pattern_str) in
     let new_digit_output = 
-        String.split_on_char ' ' digit_output_str in
+        String.split_on_char ' ' (String.trim digit_output_str) in
     (new_pattern :: accu_patterns), (new_digit_output :: accu_digit_outputs)
 
 let parse_input_lines lines =
@@ -66,21 +67,113 @@ let parse_input_lines_2 lines =
 
 (************************************************************)
 
-let decode_seven_seg seven_seg =
-    (*print_endline ("    seven_seg = " ^ seven_seg ^ ", len = " ^ (string_of_int (String.length seven_seg)));*)
-    match String.length seven_seg with
+(*
+1 -> 2 segments <---- unique
+
+2 -> 5 segments
+3 -> 5 segments
+
+4 -> 4 segments <---- unique
+
+5 -> 5 segments
+6 -> 6 segments
+
+7 -> 3 segments <---- unique
+8 -> 7 segments <---- unique
+
+9 -> 6 segments
+*)
+
+
+(* Digit is either 2, 5 or 3, figure out which with the
+   help of parameter "pattern" *)
+let decode_five_seg_digit digit pattern =
+    let digit_one = List.find (fun p -> String.length p = 2) pattern in
+
+    (*print_endline ("decode_five_seg_digit: digit_one = " ^ digit_one);*)
+
+    (* Out of the digits 2, 5 and 3 we know that only the digit 3 contains
+       both segments that makes up the digit 1. So if the following expression
+       evaluates to true we know the digit is a three. *)
+    let is_three = List.for_all (fun seg -> String.contains digit seg) (String.to_list digit_one) in
+    if is_three then
+        3
+    else
+        (* Check how many segments overlap *)
+        let digit_four = List.find (fun p -> String.length p = 4) pattern in
+        let overlapping_segs = List.count ~f:(fun seg -> String.contains digit seg) (String.to_list digit_four) in
+        match overlapping_segs with
+        | 3 -> 5 (* Digit 5 overlaps with 3 of the segments of digit 4 *)
+        | 2 -> 2 (* Digit 2 overlaps with 2 of the segments of digit 4 *)
+        | _ ->
+            (* Wait a second. This isn't right!! *)
+            raise (Failure ("Failed decode five seg. overlapping_segs = " ^ (string_of_int overlapping_segs)))
+
+(* Digit is either 0, 6 or 9, figure out which with the
+   help of parameter "pattern" *)
+let decode_six_seg_digit digit pattern =
+    let digit_four = List.find (fun p -> String.length p = 4) pattern in
+
+    (* Out of the digits 0, 6 and 9 we know that only the digit 9 contains
+       all segments that makes up the digit 4. So if the following expression
+       evaluates to true we know the digit is a nine. *)
+    let is_nine = List.for_all (fun seg -> String.contains digit seg) (String.to_list digit_four) in
+    if is_nine then
+        9
+    else
+        (* Check how many segments overlap *)
+        let digit_one = List.find (fun p -> String.length p = 2) pattern in
+
+        (* Between the digits 0 and 6, we know that only the digit 0 overlaps
+           with all the segments of digit 1. So if the following expressions
+           evaluates to true we know the digit is a zero. *)
+        let is_zero = List.for_all (fun seg -> String.contains digit seg) (String.to_list digit_one) in
+        if is_zero then
+            0
+        else
+            6
+
+(* Decodes the seven segment digit 'digit' from a
+   string letter-pattern to a integer value.
+
+   For any digit that doesn't have a unique number
+   of segments, it will use the parameter 'pattern'
+   which is a list of seven segment letter-patterns
+   to figure out which seven segment digit the
+   parameter 'digit' should be decoded to.
+   
+   Will raise exception when it finds a digit that it fails to decode. *)
+let decode_seven_seg digit pattern =
+    (*print_endline ("    seven_seg = " ^ digit ^ ", len = " ^ (string_of_int (String.length digit)));*)
+    match String.length digit with
+    | 2 -> 1 (* 1 on seven segment display uses 2 segments *)
+    | 3 -> 7 (* 7 on seven segment display uses 3 segments *)
+    | 4 -> 4 (* 4 on seven segment display uses 4 segments *)
+    | 5 ->
+        (* The digits 2, 3 and 5 all use 5 segments *)
+        decode_five_seg_digit digit pattern
+    | 6 ->
+        (* The digits 0, 6 and 9 all use 6 segments *)
+        decode_six_seg_digit digit pattern
+    | 7 -> 8 (* 8 on seven segment display uses 7 segments *)
+    | _ ->
+        raise (Failure ("Cannot parse input digit value: " ^ digit))
+
+let decode_unique_seven_seg digit =
+    (*print_endline ("    seven_seg = " ^ digit ^ ", len = " ^ (string_of_int (String.length digit)));*)
+    match String.length digit with
     | 2 -> 1 (* 1 on seven segment display uses 2 segments *)
     | 3 -> 7 (* 7 on seven segment display uses 3 segments *)
     | 4 -> 4 (* 4 on seven segment display uses 4 segments *)
     | 7 -> 8 (* 8 on seven segment display uses 7 segments *)
-    | _ ->
-        0
-        (*raise (Failure ("Cannot parse input seven_seg value: " ^ seven_seg))*)
+    | _ -> -1
 
 (* "Unique digit" means a seven segment digit with a unique number of segments.
    Those digits happens to be: 1, 4, 7 and 8. *)
 let is_unique_digit digit =
-    List.mem (decode_seven_seg digit) [1;4;7;8]
+    List.mem (decode_unique_seven_seg digit) [1;4;7;8]
+
+(************************************************************)
 
 let part1 lines =
     let _, digit_outputs = parse_input_lines lines in
@@ -95,6 +188,30 @@ let part1 lines =
     let answer = List.fold_left count_unique_digits 0 digit_outputs in
     print_endline ("Part 1 answer = " ^ (string_of_int answer))
 
+let part2 lines =
+    let patterns, digit_outputs = parse_input_lines lines in
+    let sum_digit_outputs accu_sum pattern digit_output =
+        (*let inner (accu_output, pattern) digit =
+            let output_value = decode_seven_seg digit pattern in
+            accu_output ^ (string_of_int output_value)
+        in
+        let output_str = List.fold_left inner ("", pattern) digit_output in
+        accu_sum + (int_of_string output_str)*)
+        let inner accu_output digit =
+            (*print_string ("GGDGGD digit = " ^ digit ^ " pattern = ");*)
+            (*List.iter (fun p -> print_string (p ^ " ")) pattern;*)
+            (*print_endline "";*)
+            let output_value = decode_seven_seg digit pattern in
+            accu_output ^ (string_of_int output_value)
+        in
+        let output_str = List.fold_left inner "" digit_output in
+        (*print_endline ("output_str = " ^ output_str ^ "\n");*)
+        accu_sum + (int_of_string output_str)
+    in
+    let answer = List.fold_left2 sum_digit_outputs 0 patterns digit_outputs in
+    print_endline ("Part 2 answer = " ^ (string_of_int answer))
+
 let () = 
     let lines = input_all_lines "input.txt" in
-    part1 lines
+    part1 lines;
+    part2 lines
